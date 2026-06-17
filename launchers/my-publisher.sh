dc2a4a20289c7c2116f96753d79ae97662f5e228
Nayma Hassan <naymahassan@Naymas-MacBook-Pro.local>
2026-06-17 12:46:41 -0600

    Initial commit: Added custom camera nodes and config

---
+++ launchers/my-publisher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# launch publisher
+rosrun my_package my_publisher_node.py
+
+# wait for app to end
+dt-launchfile-join
